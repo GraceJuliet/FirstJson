3741e178e441e95c108c21182f76c9190021dbf0
GraceJuliet <71426771+GraceJuliet@users.noreply.github.com>
2021-03-24 19:00:49 +0530

    Add files via upload

---
+++ FirstJson/FirstJson/Models/RelayCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstJson.Models
+{
+    class RelayCommand
+    {
+    }
+}
---
+++ FirstJson/FirstJson/ViewModels/ViewModel.cs
@@ -0,0 +1,79 @@
+﻿using FirstJson.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FirstJson.ViewModels
+{
+     public class ViewModel:INotifyPropertyChanged
+    {
+        public Employee emp;
+
+        public RelayCommand Cmd { get; set; }
+
+
+        Item it;
+
+        public ViewModel()
+        {
+            emp = new Employee();
+            Cmd = new RelayCommand(Canexecute, CanUse);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+
+
+        private string address;
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+                OnPropertyChanged("Address");
+            }
+        }
+
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
