652cc6acfaa25467489b9372dfdd3609a0bd65ed
GraceJuliet <71426771+GraceJuliet@users.noreply.github.com>
2021-03-25 18:18:50 +0530

    Add files via upload

---
+++ FirstJson/Models/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FirstJson.Models
+{
+    public class Employee
+    {
+        public List<Item> items { get; set; }
+
+
+
+
+    }
+        public class Item
+        {
+            private string name;
+
+            public string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+
+            private string id;
+
+            public string Id
+            {
+                get
+                {
+                    return id;
+                }
+                set
+                {
+                    id = value;
+                }
+            }
+
+
+            private string address;
+
+            public string Address
+            {
+                get
+                {
+                    return address;
+                }
+                set
+                {
+                    address = value;
+                }
+            }
+        }
+
+           
+    
+}
